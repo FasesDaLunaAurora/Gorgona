4dd1aec517e3af37ba3d1ae9c9b50d52d5215a81
Luna Aurora <108474761+FasesDaLunaAurora@users.noreply.github.com>
2025-10-18 11:06:18 -0300

    Refactor database connection setup, add Usuario model, and update Docker configuration for Alembic

--- backend/microsservico_autenticacao/src/database.py
+++ backend/microsservico_autenticacao/src/database.py
@@ -1,14 +1,14 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
-Base = declarative_base()   
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL não está definida no container")
+
+engine = create_engine(DATABASE_URL)  
+Base = declarative_base() 
 
 def pegar_sessao():
     try:
---
+++ backend/microsservico_autenticacao/src/models/auth_models.py
@@ -0,0 +1,20 @@
+
+from sqlalchemy import Column, Integer, String, Boolean
+from src.database import Base
+
+class Usuario(Base):
+    __tablename__ = 'usuarios'
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    nome = Column("nome", String, unique=True, nullable=False)
+    email = Column("email", String, unique=True, nullable=False)
+    senha = Column("senha", String, nullable=False)
+    ativo = Column("ativo", Boolean, default=True)
+    admin = Column("admin", Boolean, default=False)
+
+    def __init__(self, nome, email, senha, ativo=True, admin=False):
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+        self.ativo = ativo
+        self.admin = admin

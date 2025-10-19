6463faa174e3798731ade8bba19e23e5e8dc74f0
Luna Aurora <108474761+FasesDaLunaAurora@users.noreply.github.com>
2025-10-19 11:03:06 -0300

    Implement user authentication and creation functionality: add login route, refactor user creation logic, and enhance user service with token generation and password verification.

--- backend/microsservice_user/src/routers/user_routers.py
+++ backend/microsservice_user/src/routers/user_routers.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, Depends, HTTPException
 from src.database import pegar_sessao
 from sqlalchemy.orm import Session
-from src.schemas.user_schemas import UsuarioSchema
-from src.models.user_models import UsuarioModel
+from src.schemas.user_schemas import UsuarioSchema, LoginSchema
 from src.services.user_services import UsuarioService
+from datetime import timedelta
 
 
 user_router = APIRouter(prefix="/user", tags=["user"])
@@ -13,9 +13,26 @@ async def criar_usuario(schema: UsuarioSchema, sessao: Session = Depends(pegar_s
     '''
     Rota de criação de usuário
     '''
-    usuario = sessao.query(UsuarioModel).filter(UsuarioModel.email == schema.email).first()
+    usuario = UsuarioService.buscar_usuario_email(sessao, schema.email, schema.senha)
     if usuario:
         raise HTTPException(status_code=409, detail="Email já cadastrado")
     else:
         novo_usuario = UsuarioService.criar_usuario(sessao, schema.nome, schema.email, schema.senha, schema.ativo, schema.admin)
         return {"mensagem": f"Usuário {novo_usuario.nome} criado com sucesso"}
+    
+@user_router.post("/login")
+async def login(schema: LoginSchema, sessao: Session = Depends(pegar_sessao)):
+    '''
+    Rota de login de usuário
+    '''
+    usuario = UsuarioService.autenticar_usuario(sessao, schema.email, schema.senha)
+    if not usuario:
+        raise HTTPException(status_code=401, detail="Usuário ou credenciais inválidas")
+    else: 
+        access_token = UsuarioService.gerar_token(usuario.id)
+        refresh_token = UsuarioService.gerar_token(usuario.id, duracao=timedelta(days=7))
+        return {
+            "access_token": access_token, 
+            "refresh_token": refresh_token,
+            "token_type": "Bearer"
+            }
--- backend/microsservice_user/src/services/user_services.py
+++ backend/microsservice_user/src/services/user_services.py
@@ -1,15 +1,50 @@
 from sqlalchemy.orm import Session
 from src.models.user_models import UsuarioModel
-from src.utils import criptografar_senha
+from passlib.context import CryptContext
+from jose import JWTError, jwt
+from datetime import datetime, timedelta, timezone
+import os
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES= os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 class UsuarioService:
 
     @staticmethod
     def criar_usuario(sessao: Session, nome: str, email: str, senha: str, ativo: bool = True, admin: bool = False):
         
-        senha_criptografada = criptografar_senha(senha)
+        senha_criptografada = bcrypt_context.hash(senha)
         novo_usuario = UsuarioModel(nome=nome, email=email, senha=senha_criptografada, ativo=ativo, admin=admin)
         sessao.add(novo_usuario)
         sessao.commit()
         sessao.refresh(novo_usuario)
         return novo_usuario
+    
+    @staticmethod
+    def buscar_usuario_email(sessao: Session, email: str):
+        usuario = sessao.query(UsuarioModel).filter(UsuarioModel.email == email).first()
+        if not usuario:
+            return False
+        return usuario
+    
+    @staticmethod
+    def autenticar_usuario(sessao: Session, email: str, senha: str):
+        usuario = sessao.query(UsuarioModel).filter(UsuarioModel.email == email).first()
+        if not usuario:
+            return False
+        elif not bcrypt_context.verify(senha, usuario.senha):
+            return False
+        return usuario
+    
+    @staticmethod
+    def gerar_token(user_id: int, duracao = timedelta(minutes= int(ACCESS_TOKEN_EXPIRE_MINUTES))):
+        data_expiracao = datetime.now(timezone.utc) + duracao
+        jwt_codificado = {
+            "sub": str(user_id),
+            "exp": data_expiracao
+        }
+        token = jwt.encode(jwt_codificado, SECRET_KEY, algorithm=ALGORITHM)
+        return token
